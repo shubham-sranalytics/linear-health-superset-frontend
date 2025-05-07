5ca5df32b298469951a3527411e8954bd080e421
Shubham More <shubham.m@sranalytics.io>
2025-05-07 13:16:17 +0530

    feat: update SupersetEmbed component with new dashboard ID and guest token

--- src/App.vue
+++ src/App.vue
@@ -3,5 +3,9 @@ import SupersetEmbed from './components/SupersetEmbed.vue'
 </script>
 
 <template>
-  <SupersetEmbed dashboard-id="ddfceaa6-c2a7-4e91-99d1-030c68107778" guest-token="" />
+  <SupersetEmbed
+    dashboard-id="30ddf642-4c36-40ee-ade2-fc77e6285a6c"
+    guest-token="eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7InVzZXJuYW1lIjoiIiwiZmlyc3RfbmFtZSI6IiIsImxhc3RfbmFtZSI6IiJ9LCJyZXNvdXJjZXMiOlt7InR5cGUiOiJkYXNoYm9hcmQiLCJpZCI6IjMwZGRmNjQyLTRjMzYtNDBlZS1hZGUyLWZjNzdlNjI4NWE2YyJ9XSwicmxzX3J1bGVzIjpbXSwiaWF0IjoxNzQ2NjAzOTM5LjI0NDk5NzMsImV4cCI6MTc0NjYwNDIzOS4yNDQ5OTczLCJhdWQiOiJodHRwOi8vMC4wLjAuMDo4MDgwLyIsInR5cGUiOiJndWVzdCJ9.maGmRO92HMh2SnGvIm3RZIFMkqWrsbqF7BUB9de5PP8"
+    superset-domain="http://ec2-3-142-16-81.us-east-2.compute.amazonaws.com:8088"
+  />
 </template>
