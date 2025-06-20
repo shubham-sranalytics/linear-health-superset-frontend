c983fb983250471824434374a7ed76931a81da5b
Shubham More <shubham.m@sranalytics.io>
2025-06-20 12:57:02 +0530

    Update SupersetEmbed.vue

--- src/components/SupersetEmbed.vue
+++ src/components/SupersetEmbed.vue
@@ -35,7 +35,7 @@ onMounted(() => {
           expanded: false,
           visible: false,
         },
-        urlParams: { standalone: 1, activeTab: 1 },
+        urlParams: { standalone: 3, activeTab: 1 },
         hideChartControls: true,
       },
     })
